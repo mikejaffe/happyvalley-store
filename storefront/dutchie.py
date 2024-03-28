4a6d01a6e8879daa3f87f6bde43bd5d344ce938a
Mike Jaffe <mikejaffe@Mikes-MacBook.fios-router.home>
2024-03-27 22:58:19 -0400

    dutchie api setup

--- storefront/dutchie.py
+++ storefront/dutchie.py
@@ -1,12 +1,119 @@
 import requests
+import json
+import os
 
 class Dutchie:
   def __init__(self, retailer_id, order_id):
     self.retailer_id = retailer_id
     self.order_id = order_id
 
+  def make_request(self, payload):
+    Headers = {"Authorization": os.environ.get('DUTCHIE_API_KEY')}
+    result = requests.post(url="https://plus.dutchie.com/plus/2021-07/graphql", json={"query":payload}, headers=Headers)
+    return json.loads(result.text) 
+
+  def fetch_info(self):
+    payload = ("""query { retailer(id: "%s") {
+          address
+          name
+          menuTypes
+          banner {
+            html
+          }
+          paymentOptions {
+            aeropay
+            alt36
+            canPay
+            cashless
+            cashOnly
+            check
+            creditCard
+            creditCardAtDoor
+            creditCardByPhone
+            debitOnly
+            hypur
+            linx
+            merrco
+            payInStore
+            paytender
+          }
+          deliverySettings {
+            afterHoursOrderingForDelivery
+            afterHoursOrderingForPickup
+            deliveryArea
+            deliveryFee
+            deliveryMinimum
+            disablePurchaseLimits
+            limitPerCustomer
+            pickupMinimum
+            scheduledOrderingForDelivery
+            scheduledOrderingForPickup
+          }
+          description
+          fulfillmentOptions {
+           curbsidePickup
+            delivery
+            driveThruPickup
+            pickup
+          }
+          hours {
+              specialHours {
+              startDate
+              endDate
+              name
+              specialOperatingHours {
+                date
+                pickup {
+                  active
+                  start
+                  end
+                }
+              }
+            }
+            pickup {
+              Monday {
+                start
+                end
+                active
+              }
+              Tuesday {
+          start
+          end
+          active
+            }
+            Wednesday {
+              start
+              end
+              active
+            }
+            Thursday {
+              start
+              end
+              active
+            }
+            Friday {
+              start
+              end
+              active
+            }
+            Saturday {
+              start
+              end
+              active
+            }
+            Sunday {
+              start
+              end
+              active
+            }
+          }
+        }
+      }
+ } """ % ( self.retailer_id))
+    return self.make_request(payload)
+
   def fetch_cart(self):
-    payload = ("""query {
+    payload = ("""{
          checkout(id: %s
                   retailerId: %s
          ) {
@@ -49,5 +156,4 @@ class Dutchie:
            redirectUrl
            }
          }""" % ( self.order_id, self.retailer_id))
-
-    pass
+    return make_request(payload)
